942e84f2a39baedaf57b00c84fed5865f0baa275
beantownbabe <156287695+beantownbabe@users.noreply.github.com>
2024-05-15 14:32:25 +0200

    Changed on_message

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import chainlit as cl
 
 
-@cl.on_chat_start
-async def on_chat_start():
+@cl.on_message
+async def on_message():
     await cl.Message(content="Hi, I'm Social Robo and I'd like to help you find out more about the Social Map of Berlin.").send()
 
     
